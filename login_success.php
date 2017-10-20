aedf2658e904eff88f8b120716af7b17845f4514
rohail <rohailkhaniazi@gmail.com>
2017-10-21 02:21:33 +0500

    1st comment

---
+++ login_success.php
@@ -0,0 +1,19 @@
+
+<?php
+//session_start();
+
+//if(!isset($_SESSION["myusername"])){
+//header("location:main_login.php");
+//}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Login Successful</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
